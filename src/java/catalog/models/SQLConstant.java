6853f3aba53a78fca836935dc1b78c7d090c044e
zakafir <zak.afir@gmail.com>
2018-03-14 11:02:37 +0100

    Constants

--- src/java/catalog/models/SQLConstant.java
+++ src/java/catalog/models/SQLConstant.java
@@ -10,37 +10,44 @@ package catalog.models;
  * @author zakaria_afir
  */
 public class SQLConstant {
-    /** 
-    * Author queries   
-    */ 
+
+    /**
+     * Author queries
+     */
     protected static final String SELECT_ALL_AUTHORS = "SELECT * FROM author";
     protected static final String SELECT_ALL_AUTHORS_BY_ID = "SELECT * FROM "
             + "author WHERE author_id = ?";
     protected static final String SELECT_ALL_AUTHORS_BY_NAME = "SELECT * FROM "
             + "author WHERE author_firstname = ? OR author_lastname = ?";
-    
-//    protected static final String UPDATE_USER_BY_ID = "UPDATE author SET author_firstname = ? AND "
-//            + "author_lastname = ? WHERE author_id = ?";
-//    
-    /** 
-    * Book queries   
-    */ 
-    protected static final String SELECT_ALL_BOOKS = "SELECT * FROM book"; 
-    protected static final String INSERT_BOOK = "INSERT INTO BOOK (isbn," 
-             + "book_title,book_description,book_price,publication_date," 
-             + "catalog_id,editor_id) VALUES(DEFAULT," 
-             + " '')"; 
-     
-   /** 
-    * Members queries   
-    */ 
-    protected static final String INSERT_MEMBER = "INSERT INTO MEMBER " 
-            + "(member_id,member_lastname, member_firstname,member_email," 
-            + "member_address,member_password) VALUES(DEFAULT, ?,?,?,?,'')"; 
-     
-    /** 
-     * Connection queries 
-     */     
-    protected static final String SELECT_MEMBER = "select * from MEMBER where " 
+
+    protected static final String CREATE_AUTHOR = "INSERT INTO author"
+            + "(author_id, author_lastname, author_firstname) VALUES (DEFAULT,?,?)";
+
+    protected static final String UPDATE_USER_BY_ID = "UPDATE author SET author_lastname = ? AND "
+            + "author_firstname = ? WHERE author_id = ?";
+
+    protected static final String DELETE_AUTHOR = "DELETE author WHERE "
+            + "author_id = ?";
+
+    /**
+     * Book queries
+     */
+    protected static final String SELECT_ALL_BOOKS = "SELECT * FROM book";
+    protected static final String INSERT_BOOK = "INSERT INTO BOOK (isbn,"
+            + "book_title,book_description,book_price,publication_date,"
+            + "catalog_id,editor_id) VALUES(DEFAULT,"
+            + " '')";
+
+    /**
+     * Members queries
+     */
+    protected static final String INSERT_MEMBER = "INSERT INTO MEMBER "
+            + "(member_id,member_lastname, member_firstname,member_email,"
+            + "member_address,member_password) VALUES(DEFAULT, ?,?,?,?,'')";
+
+    /**
+     * Connection queries
+     */
+    protected static final String SELECT_MEMBER = "select * from MEMBER where "
             + "member_email=? and member_password=?";
 }
